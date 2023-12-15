08949efb9eda54ac6b782fbd97b0d4940f2cd12b
au-lex <aulexc4d2@gmail.com>
2023-12-15 17:02:34 +0100

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,31 +4,8 @@
 
 
 
-  const login = document.querySelector('.login');
-  const loginCon = document.querySelector('.loginModal');
 
-  const signup = document.querySelector('.Sign-up');
-  const signupCon = document.querySelector('.signUPModal');
 
-  const closeButtons = document.querySelectorAll('.close');
-
-  login.addEventListener('click', () => {
-    loginCon.classList.add('log');
-    signupCon.classList.remove('sig');
-  });
-
-  signup.addEventListener('click', () => {
-    signupCon.classList.add('sig');
-    loginCon.classList.remove('log');
-  });
-
-  {/* // Add event listeners for close buttons */}
-  closeButtons.forEach((closeButton) => {
-    closeButton.addEventListener('click', () => {
-      loginCon.classList.remove('log');
-      signupCon.classList.remove('sig');
-    });
-  });
 
 // FAQ Starts
 function faq() {
